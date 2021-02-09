acc27881258879d211aecb2fa944e4ba00a09cef
Christian Westgaard <cwe@enonic.com>
2021-02-09 09:23:44 +0100

    Moved from enonic/enonic-npm-modules

--- src/Input.jsx
+++ src/Input.jsx
@@ -2,23 +2,19 @@ import {getIn} from 'formik';
 import {Input as SemanticUiReactInput} from 'semantic-ui-react';
 
 
-export const Input = ({
-	type = 'text',
-	name,
-	parentPath,
-	path = parentPath ? `${parentPath}.${name}` : name,
-	formik: {
-		setFieldValue,
-		values
-	},
-	onChange = (event, {
-		value: newValue
-	}) => {
-		setFieldValue(path, newValue);
-	},
-	value = getIn(values, path, ''),
-	...rest
-}) => {
+export const Input = (props) => {
+	//console.debug('Input props', props);
+	const {
+		type = 'text',
+		name,
+		parentPath,
+		path = parentPath ? `${parentPath}.${name}` : name,
+		formik: {setFieldValue,	values},
+		onChange = (event, {value: newValue}) => setFieldValue(path, newValue),
+		value = getIn(values, path, ''),
+		...rest
+	} = props;
+
 	return <SemanticUiReactInput
 		name={path}
 		onChange={onChange}
